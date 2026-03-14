01fe9d2a8e7fb5b6a4b59935250b1f078f8b5f8c
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-03-13 23:12:51 -0300

    feat: entity empreendimento & enum

---
+++ src/main/java/com/sc/empreende/api/entities/Empreendimentos.java
@@ -0,0 +1,45 @@
+package com.sc.empreende.api.entities;
+
+import java.util.UUID;
+
+import com.sc.empreende.api.shared.SegmentoAtuacao;
+import com.sc.empreende.api.shared.StatusEmpreendimento;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_empreendimento")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class Empreendimentos {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "nome_empreendimento", nullable = true)
+    private String nomeEmpreendimento;
+
+    @Column(name = "nome_responsavel", nullable = false)
+    private String nomeResponsavel;
+
+    @Column(name = "municipio", nullable = false)
+    private String municipio;
+
+    @Column(name = "segmento", nullable = false)
+    private SegmentoAtuacao segmento;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "status", nullable = false)
+    private StatusEmpreendimento status;
+}
